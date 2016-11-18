61a8e0ff5a761049aea6fe74844f2a2a4158517d
Lars Gröber <larsgroeber7@gmail.com>
2016-11-19 00:33:03 +0100

    improved index.php

--- testProjekt/index.php
+++ testProjekt/index.php
@@ -1,21 +1,29 @@
-<form action="#" method="POST">
-  <input type="text" name="user">
-  <br>
-  <input type="password" name="pass">
-  <br>
-  <button type="submit" name="Submit">Submit</button>
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Workshop - Testprojekt</title>
+  </head>
+  <body>
+    <form action="#" method="POST">
+    <input type="text" name="user">
+    <br>
+    <input type="password" name="pass">
+    <br>
+    <button type="submit" name="Submit">Submit</button>
+  </form>
 
-<?php
-  if ( isset( $_POST ) )
-  {
-    if ( $_POST["user"] == "me" && $_POST["pass"] == "pass" )
+  <?php
+    if ( isset( $_POST ) )
     {
-      echo "Success";
+      if ( $_POST["user"] == "me" && $_POST["pass"] == "pass" )
+      {
+        echo "Success";
+      }
+      else
+      {
+        echo "Fail";
+      }
     }
-    else
-    {
-      echo "Fail";
-    }
-  }
-?>
+  ?>
+  </body>
+</html>
